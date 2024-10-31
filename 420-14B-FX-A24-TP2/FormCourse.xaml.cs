6f3f425c547381c4bb4013d26fe38001d9de03e0
Amine Blal <m.amine.blal@edu.cegepgarneau.ca>
2024-10-31 19:00:57 -0400

    Création du Formulaire de Course avec le TabItem sur les infos et Coureurs

---
+++ 420-14B-FX-A24-TP2/FormCourse.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _420_14B_FX_A24_TP2
+{
+    /// <summary>
+    /// Logique d'interaction pour FormCourse.xaml
+    /// </summary>
+    public partial class FormCourse : Window
+    {
+        public FormCourse()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAjout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnModifier_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnSupprimer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnAnnuler_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAjoutCoureurs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnModifierCoureurs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSupprimerCoureurs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
