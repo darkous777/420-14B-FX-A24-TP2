b753c5e828b134a5c0b7c2b1b5ba635ba1557e84
Amine Blal <m.amine.blal@edu.cegepgarneau.ca>
2024-11-02 17:57:20 -0400

    Ajout de course dans la liste

--- 420-14B-FX-A24-TP2/classes/GestionCourse.cs
+++ 420-14B-FX-A24-TP2/classes/GestionCourse.cs
@@ -30,7 +30,7 @@ namespace _420_14B_FX_A24_TP2.classes
             ChargerCourse(cheminFichierCoursesCsv, cheminFichierCoureursCsv);
         }
         /// <summary>
-        /// Méthode permettant de vérifier si une course est déjà existante dans la liste de course
+        /// Méthode permettant de vérifier si une course est déjà existante dans la liste de courses
         /// </summary>
         /// <param name="course">Course à vérifier si existante</param>
         /// <returns>Si la course est existante dans la liste de course (true/false)</returns>
@@ -42,11 +42,28 @@ namespace _420_14B_FX_A24_TP2.classes
 
             foreach (Course c in Courses)
             {
-                if (c.Id == course.Id)
+                if (c.Nom == course.Nom && c.Date == course.Date)
                     return true;
             }
 
             return false;
+        }
+        /// <summary>
+        /// Ajoute une course dans la liste de course
+        /// </summary>
+        /// <param name="course">Course à vérifier si existante sinon ajouter dans la liste</param>
+        /// <exception cref="ArgumentNullException">Excecption retournant une exception si nulle</exception>
+        /// <exception cref="InvalidOperationException">Exception retournant une exception si existante</exception>
+        public void AjouterCourse(Course course)
+        {
+            if (course is null)
+                throw new ArgumentNullException(nameof(course), "La course ne peut être nul!");
+
+            if (Existe(course))
+                throw new InvalidOperationException("Impossible d'ajouter la course, car elle existe déjà dans la liste!");
+
+
+            Courses.Add(course);
         }
        
     }
